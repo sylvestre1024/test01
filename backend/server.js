4ee8de073bc3d28c042360d560d940e2f4f93721
Sylvestre <sylvestre1024@gmail.com>
2022-07-25 22:02:12 +0200

    Maj server.js et valeur du port en dur

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,61 @@
 const http = require('http');
 const app = require('./app');
 
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
 /*
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur3 !');
-});
+//la fonction normalizePort renvoie un port valide, 
+// qu'il soit fourni sous la forme d'un numéro ou d'une chaîne ;
+const port = normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
 */
-// sur quel port
-app.set('port', process.env.PORT || 3000);
+app.set('port', '3000');
+
+//la fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. 
+// Elle est ensuite enregistrée dans le serveur ;
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+// un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé 
+// sur lequel le serveur s'exécute dans la console.
+server.on('error', errorHandler);
+
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + '3000';
+  console.log('Listening on ' + bind);
+});
+
+server.listen('3000');
+
